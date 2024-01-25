db4e422bcd4b54b226a8f8ab96d7cc2b5dba2ab3
v-homyakov <v-homyakov@yandex-team.ru>
2024-01-25 17:20:10 +0400

    Fix: ignore constructors, methods and getters

---
+++ test/cases/function-expressions/input.js
@@ -0,0 +1,8 @@
+const a1 = function(a) {};
+const a2 = (function(a) {});
+
+const b1 = a => {};
+const b2 = (a => {});
+
+const c1 = function(c = () => {}) {};
+const c2 = (function(c = () => {}) {});
---
+++ test/cases/object-fields/input.js
@@ -0,0 +1,10 @@
+const b = {
+  prop1: (function () {return ''})(),
+  prop2: function () {return ''},
+  prop3: () => {},
+  prop4: (() => {}),
+  12: () => {},
+  13: (() => {}),
+  test () {},
+  get rect () {},
+}
